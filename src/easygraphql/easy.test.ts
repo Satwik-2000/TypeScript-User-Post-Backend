c0758f9d6e035a0d6cc99de97e4ebdaeb217af96
Satwik Ghosh <satwikghosh2000@gmail.com>
2021-11-01 10:59:03 +0530

    Incorporated Schema.gql into EasyGraphqlTester

--- src/easygraphql/easy.test.ts
+++ src/easygraphql/easy.test.ts
@@ -1,13 +1,45 @@
-const EasyGraphqlLoadTester = require('easygraphql-load-tester');
+const EasyGraphqlTester = require('easygraphql-tester');
 const fs = require('fs');
 const path = require('path');
 
-const userSchema = fs.readFileSync(
-    path.join(__dirname, "src/graphql/schema", "/user.graphql"), "utf8"
+// const userSchema = fs.readFileSync(
+//     path.join(__dirname, "src/graphql/schema", "/user.graphql"), "utf8"
+// )
+
+// const postSchema = fs.readFileSync(
+//     path.join(__dirname, "src/graphql/schema", "/post.graphql"), "utf8"
+// )
+
+// const tester = new EasyGraphqlTester([userSchema, postSchema]);
+
+// describe("Test The Schema, Query", () => {
+//     let userTester:any, postTester:any;
+//     beforeAll(() => {
+//         userTester = new EasyGraphqlTester(userSchema);
+//         postTester = new EasyGraphqlTester(postSchema);
+//     })
+// })
+
+const schemaCode = fs.readFileSync(
+    path.join(__dirname, "..", "schema.gql"), "utf8"
 )
 
-const postSchema = fs.readFileSync(
-    path.join(__dirname, "src/graphql/schema", "/post.graphql"), "utf8"
-)
+describe("Testing Schema, Query", () => {
+    let tester;
+    beforeAll(() => {
+        tester = new EasyGraphqlTester(schemaCode);
+    });
 
-const tester = new EasyGraphqlLoadTester([userSchema, postSchema]);
+    describe("Queries", () => {
+        test("Should Pass with Query", () => {
+            const query = `
+            {
+                testInfo(user: "username") {
+                    valueOne
+                    valueTwo
+                }
+            }`
+            tester.test(true, query);
+        });
+    });
+});
